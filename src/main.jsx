f758cdd3df9b1aaec4766387015eaa69ddebce55
Hammad Ali <hammadali78056@gmail.com>
2025-08-04 09:15:39 +0500

    feat: creating header component and react routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter , createBrowserRouter,  createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* <Route path="about" element={<About />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="github" element={<Github />} /> */}
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
